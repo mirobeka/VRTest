7898643c6a18333fcbe7724b210ada5b8df5d60c
Miroslav Beka <mirobeka@gmail.com>
2021-05-07 09:58:28 +0200

    Autodestroy object after explosion effect

---
+++ Assets/Scripts/AutoDestroyParticleEffect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyParticleEffect : MonoBehaviour
+{
+    private ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ps != null)
+        {
+            if(!ps.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+}
