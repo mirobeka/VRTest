db01ae0c4688ce74497cd19b98464e1582a8dffb
Miroslav Beka <mirobeka@gmail.com>
2021-09-07 16:25:32 +0200

    Svetielko na gombik

--- Assets/PhysicsButton.cs
+++ Assets/PhysicsButton.cs
@@ -6,17 +6,23 @@ public class PhysicsButton : MonoBehaviour
     [SerializeField] private float threshold = 0.1f;
     [SerializeField] private float deadZone = 0.025f;
 
-    public UnityEvent onPressed, onReleased;
+    public UnityEvent onPressed, onReleased = null;
+    public Material lightOnMaterial = null;
 
-    private bool isPressed;
-    private Vector3 startPos;
-    private ConfigurableJoint joint;
+    private Material originalMaterial = null;
+    private GameObject clicker = null;
+    private bool lightOn = false;
+    private bool isPressed = false;
+    private Vector3 startPos = Vector3.zero;
+    private ConfigurableJoint joint = null;
+
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.localPosition;
         joint = GetComponent<ConfigurableJoint>();
-        
+        clicker = transform.Find("Clicker").gameObject;
+        originalMaterial = clicker.GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
@@ -49,5 +55,18 @@ public class PhysicsButton : MonoBehaviour
         isPressed = false;
         onReleased.Invoke();
         Debug.Log("Released");
+    }
+
+    public void ToggleLight(){
+        lightOn = !lightOn;
+
+        if (lightOnMaterial == null)
+            return;
+
+        if (lightOn){
+            clicker.GetComponent<MeshRenderer>().material = lightOnMaterial;
+        }else{
+            clicker.GetComponent<MeshRenderer>().material = originalMaterial;
+        }
     }
 }
